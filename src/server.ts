47f4d6bda7c0610a904204c7b2a0988bdd19abe3
eslamahmed74 <islamaboelmaged74@gmail.com>
2025-06-02 16:06:06 +0300

    fix the deployment error

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
 import app from './app';
+const PORT = process.env.PORT || 3000;
 
-app.listen('3000', () => console.log('server is running on port 3000'));
+app.listen(PORT, () => console.log('server is running on port 3000'));
