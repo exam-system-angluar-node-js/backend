e02c45fb9bf231e737453363114549f8b3edae4f
Mohamed Elgendy <93053571+gendyvip@users.noreply.github.com>
2025-06-07 06:57:39 +0200

    Add delete-user handler and route endpoint

--- src/routes/auth-routes.ts
+++ src/routes/auth-routes.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import { loginValidators, signupValidators } from '../utils/signupValidators';
 import { validateRequest } from '../middlewares/validateRequest';
 import { protect } from '../middlewares/protect';
-import { updateProfile } from '../handlers/auth-handler';
+import { updateProfile, deleteAccount } from '../handlers/auth-handler';
 
 import {
   currentUser,
@@ -18,6 +18,7 @@ router.post('/login', loginValidators, validateRequest, loginHandler);
 router.post('/signup', signupValidators, validateRequest, signupHandler);
 router.get('/profile', protect, currentUser);
 router.patch('/me', protect, updateProfile);
+router.delete('/me', protect, deleteAccount);
 // Route to get a user by ID (admin and teacher only)
 router.get('/:userId', protect, restrictTo('admin', 'teacher'), getUserByIdHandler);
 
